20f9c98ce599cd607139d9918a9027331009e9d0
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-02 15:20:30 -0500

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :recipes
   resources :items
   resources :lists
   resources :users
- 
+  resources :sessions, only: [:create]
+  delete '/sessions', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
 end
