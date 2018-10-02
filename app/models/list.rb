326534d338713f157c724ba87065f6ff3f571304
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-01 20:54:31 -0500

    remove tmp files

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,13 @@ class List < ApplicationRecord
     has_many :items, through: :list_items
     has_many :user_lists
     has_many :users, through: :user_lists
+
+    # Here we want to 
+    def items_attributes=(item_hash)
+        byebug
+        item_hash.value.each do |item|
+            self.item = Item.find_or_create_by(name: item[:name])
+            self.item.update(item)
+        end
+    end
 end
