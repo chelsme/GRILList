b9d26adb0133f8f1aa3648876ed2c127d50607ca
github username <github email address>
2018-10-05 09:55:19 -0500

    create default top secret recipe image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_222228) do
+ActiveRecord::Schema.define(version: 2018_10_05_133942) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_222228) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "instructions"
+    t.string "img_url"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
