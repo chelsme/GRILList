c1f2c8297588778dfdbb708577667e8e6b0071af
github username <github email address>
2018-10-05 08:56:56 -0500

    change error to alert on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user
     else
-      flash[:error] = 'Hey! You got wrong username/password.'
+      flash[:alert] = 'Hey! You got wrong username/password.'
       redirect_to login_path
     end
   end
